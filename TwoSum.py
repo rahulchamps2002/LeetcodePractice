4965c9f6e1b4aced6349b6301213fe802e853831
Rahul <rahulchamps2002@gmail.com>
2025-01-14 13:37:00 -0800

    clean up

--- TwoSum.py
+++
@@ -1,8 +0,0 @@
-def twoSum(nums, target):
-    map={}
-    for i in range(len(nums)):
-        n = target - nums[i]
-        if n in map:
-            return [map[n], i]
-
-print(twoSum([2,7,11,15],9))
